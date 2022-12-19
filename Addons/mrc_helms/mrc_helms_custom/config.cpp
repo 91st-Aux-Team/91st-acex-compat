c9570e6d0cc6f4ca346aacbb1156e944aeaba065
liamcannon <liam.cannon@mymail.champlain.edu>
2022-12-18 23:49:23 -0500

    samael helmet fix

--- Addons/mrc_helms/mrc_helms_custom/config.cpp
+++ Addons/mrc_helms/mrc_helms_custom/config.cpp
@@ -65,6 +65,7 @@ class XtdGearModels
                     "Praetorian",
                     "Predator",
                     "Romeo",
+                    "Samael",
                     "Sappheron",
                     "Scorch",
                     "Silver",
@@ -249,6 +250,7 @@ class CfgWeapons
 
 	Macro_MRC_Inf_Custom_Compat_Helmet(Romeo);
 	
+    Macro_MRC_Inf_Custom_Compat_Helmet(Samael);
 	Macro_MRC_Inf_Custom_Compat_Helmet(Sappheron);
 	Macro_MRC_Inf_Custom_Compat_Helmet(Scorch);
 	Macro_MRC_Inf_Custom_Compat_Helmet(Silver);
