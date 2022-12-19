56ec14a0d2836327dcf941910c0b7b934d7d41b7
liamcannon <liam.cannon@mymail.champlain.edu>
2022-12-18 23:46:56 -0500

    chrismus

--- Addons/mrc_helms/mrc_helms_custom/config.cpp
+++ Addons/mrc_helms/mrc_helms_custom/config.cpp
@@ -35,7 +35,6 @@ class XtdGearModels
                     "Blanks",
                     "Charred",
                     "Chop",
-                    "Christmas",
                     "Chubs",
                     "Con",
                     "Cross",
