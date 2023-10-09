9d6753f1d6a439a8240bb14d100dadd600df1e70
GenericGuitar <ryangraff23@gmail.com>
2023-10-09 12:47:29 -0500

    fixed Jimmy

--- Addons/mrc_vests/config.cpp
+++ Addons/mrc_vests/config.cpp
@@ -258,6 +258,7 @@ class CfgWeapons
     MACRO_MRC_VEST_KAMA_COMPAT(Skit);
     MACRO_MRC_VEST_KAMA_COMPAT(Trill);
     MACRO_MRC_VEST_KAMA_COMPAT(Weylyn);
+    MACRO_MRC_VEST_KAMA_COMPAT(Jimmy);
 
     // OFFICER VESTS
 
@@ -305,7 +306,6 @@ class CfgWeapons
     MACRO_MRC_VEST_OFFICER_COMPAT(Mav,MRC_Officer_Vests_Custom);
     MACRO_MRC_VEST_OFFICER_COMPAT(Predator,MRC_Officer_Vests_Custom);
     MACRO_MRC_VEST_OFFICER_COMPAT(Misery,MRC_Officer_Vests_Custom);
-    MACRO_MRC_VEST_OFFICER_COMPAT(Jimmy,MRC_Officer_Vests_Custom);
 
     MACRO_MRC_VEST_OFFICER_REVERSE_COMPAT(Kuro,MRC_Officer_Vests_Custom);
     MACRO_MRC_VEST_OFFICER_REVERSE_COMPAT(Frog,MRC_Officer_Vests_Custom);
